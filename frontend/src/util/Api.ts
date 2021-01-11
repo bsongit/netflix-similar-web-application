0f0cff563d2c17112b86ab4f3a7f70f96e0c448d
besonquadros@gmail.com <besonquadros@gmail.com>
2021-01-11 04:35:57 -0300

    add changes

--- frontend/src/util/Api.ts
+++ frontend/src/util/Api.ts
@@ -1,2 +1,2 @@
 const axios = require('axios').default;
-export default axios.create({baseURL: 'http://localhost:3000/api'})
+export default axios.create({baseURL: 'http://185.212.128.162:3000/api'})
