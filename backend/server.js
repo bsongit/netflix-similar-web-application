158c8693f2a4b9046fd0dab84184a0485ab89473
besonquadros@gmail.com <besonquadros@gmail.com>
2021-01-11 04:31:53 -0300

    add changens

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const database = require('./src/config/db.js');
 const cors = require('./src/config/cors');
 const app = express();
 
-const hostname = 'localhost';
+const hostname = '185.212.128.162';
 const port = 3000;
 
 app.use(express.json({ urlencoded: false }));
